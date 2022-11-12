dc471f7b21c36688bc11df1391e56b2b64a8132d
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-11-12 18:04:42 -0500

    starting mobile inspirational quote app with expo typescript template and installed linear gradient component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <LinearGradient
+        colors={["rgba(0,0,0,0.9)", "transparent"]}
+        // style={styles.background}
+      />
+      <Text style={styles.text}>Find Your Inspiration</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "magenta",
   },
 });
