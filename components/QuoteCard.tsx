c9b35d14c7f9abbbf90b5a49ba52a4048388abf3
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-01-01 23:14:40 -0500

    increased size of quote text and added placeholder pointing emoji as swipe up indicator

--- components/QuoteCard.tsx
+++ components/QuoteCard.tsx
@@ -5,8 +5,9 @@ import { styles } from "../styles";
 export default function QuoteCard({ quote }: QuoteProp) {
   return (
     <View>
-      <Text style={styles.quoteText}>{quote?.body}</Text>
-      <Text style={styles.quoteText}>-{quote?.author}</Text>
+      <Text style={styles.swipeUpIcon}>👆🏻</Text>
+      <Text style={styles.quoteBodyText}>{quote?.body}</Text>
+      <Text style={styles.quoteAuthorText}>-{quote?.author}</Text>
     </View>
   );
 }
--- styles.js
+++ styles.js
@@ -6,38 +6,39 @@ export const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    color: "#fff0e6",
-    fontSize: 30,
-  },
-
   background: {
-    // width: "100%",
-    // height: "100%",
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
-  quoteButton: {
-    backgroundColor: "#ffcccc",
-    borderRadius: 5,
-    marginTop: 30,
-    marginBottom: 80,
-    padding: 5,
-  },
+  // quoteButton: {
+  //   backgroundColor: "#ffcccc",
+  //   borderRadius: 5,
+  //   marginTop: 30,
+  //   marginBottom: 80,
+  //   padding: 5,
+  // },
 
-  quoteButtonText: {
-    fontSize: 20,
+  // quoteButtonText: {
+  //   fontSize: 20,
+  // },
+  swipeUpIcon: {
+    textAlign: "center",
+    marginTop: 200,
   },
-  quoteText: {
+  quoteBodyText: {
     marginLeft: 50,
     marginRight: 50,
+    marginTop: 20,
     marginBottom: 10,
     color: "white",
+    fontSize: 30,
     textAlign: "center",
   },
-  sun: {
-    marginTop: 50,
-    fontSize: 100,
+  quoteAuthorText: {
+    marginRight: 30,
+    color: "white",
+    fontSize: 20,
+    textAlign: "right",
   },
 });
